ba9f871fd51da56dc7c181980499345f647a8286
kmmehta89 <kunal@gjsusa.com>
2025-09-23 16:14:57 -0400

    Update create-hubspot-contact.js

--- api/create-hubspot-contact.js
+++ api/create-hubspot-contact.js
@@ -5,13 +5,22 @@ const hubspotClient = new hubspot.Client({
 });
 
 module.exports = async function handler(req, res) {
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.gjsusa.com');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
   try {
     const { email, sessionData, conversationHistory, conversionTrigger } = req.body;
-
+    
     const contactProperties = {
       email: email,
       chatbot_sessions_count: sessionData.sessionsCount || 1,
